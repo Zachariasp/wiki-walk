ce47d3b02e05e0a946c37ecc0b712cb1a4b8fd43
Daniel Callahan <technomancer223@gmail.com>
2022-07-26 19:38:29 -0700

    Adds bone-simple method to grab links from a given wikipedia page

--- api/wiki.py
+++ api/wiki.py
@@ -6,11 +6,18 @@ blueprint = Blueprint('wiki', __name__, url_prefix='/wiki')
 @blueprint.route("/")
 def hello():
     text = "Hello, World!"
-    text += getPage()
+    text += get_links_from_page('frog')
     return text
 
-def getPage():
+def get_page():
     wiki = wikipediaapi.Wikipedia('en', wikipediaapi.ExtractFormat.HTML)
     page = wiki.page('frog')  # frog time :)
     page_text = page.text
     return page_text
+
+def get_links_from_page(title):
+    wiki = wikipediaapi.Wikipedia('en', wikipediaapi.ExtractFormat.HTML)
+    page = wiki.page(title)
+    pageLinks = str(page.links)
+
+    return pageLinks
